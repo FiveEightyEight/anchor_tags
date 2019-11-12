b3cd53997402bd2bef40e625078c0d22060c8532
Robert Abreu <robertabreu@pursuit.org>
2019-11-11 22:27:27 -0500

    testing prettier formatting

--- js/index.js
+++ js/index.js
@@ -1,24 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-    setupButton();
-})
+document.addEventListener('DOMContentLoaded', () => {
+	setupButton();
+});
 
 const setupButton = () => {
-    const submit = document.getElementById("submit");
-    submit.addEventListener("click", () => {
-        createAnchor()
-    });
+	const submit = document.getElementById('submit');
+	submit.addEventListener('click', () => {
+		createAnchor();
+	});
 };
 
 const createAnchor = () => {
-    const input = document.getElementById("input");
-    const content = document.getElementById("content");
-    const value = input.value;
-    input.value = "";
-    const a = document.createElement("a");
-    const div = document.createElement("div");
-    a.innerText = "Link to " + value + " picture";
-    a.target = "_blank";
-    a.href = "picture.html?id=" + value;
-    div.appendChild(a);
-    content.appendChild(div);
+	const input = document.getElementById('input');
+	const content = document.getElementById('content');
+	const value = input.value;
+	input.value = '';
+	const a = document.createElement('a');
+	const div = document.createElement('div');
+	a.innerText = 'Link to ' + value + ' picture';
+	a.target = '_blank';
+  	a.href = 'picture.html?id=' + value;
+	div.appendChild(a);
+	content.appendChild(div);
 };
