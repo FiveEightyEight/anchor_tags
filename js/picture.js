1476e27c34045940e6efd801c314d1adbaace5b7
Robert Abreu <robertabreu@pursuit.org>
2019-11-11 22:43:40 -0500

    more prettier config

--- js/picture.js
+++ js/picture.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Page has loaded")
-    const query = parseURL();
-    loadImage(query);
-    modifyTitle(query);
-})
+	console.log("Page has loaded");
+	const query = parseURL();
+	loadImage(query);
+	modifyTitle(query);
+});
 
 const parseURL = () => {
-    if(!document.URL.includes("?")) {
-        // display an error
-        return;
-    };
+	if (!document.URL.includes("?")) {
+		// display an error
+		return;
+	}
 
-    /*
+	/*
     url: file:///Users/robertabreu/Desktop/Development/JavaScript/playGround/anchor_tag/picture.html?id=cat
     split at "?"
     [file:///Users/robertabreu/Desktop/Development/JavaScript/playGround/anchor_tag/picture.html, id=cat][1]
@@ -21,18 +21,18 @@ const parseURL = () => {
     cat
     */
 
-    const query = document.URL.split("?")[1].split("=")[1]
-    return query;
+	const query = document.URL.split("?")[1].split("=")[1];
+	return query;
 };
 
-const loadImage = (query) => {
-    const content = document.getElementById("content")
-    const img = document.createElement("img");
-    img.src = "https://source.unsplash.com/1600x900/?" + query;
-    content.appendChild(img);
+const loadImage = query => {
+	const content = document.getElementById("content");
+	const img = document.createElement("img");
+	img.src = "https://source.unsplash.com/1600x900/?" + query;
+	content.appendChild(img);
 };
 
-const modifyTitle = (newTitle) => {
-    const title = document.getElementById("title");
-    title.innerText = "Pictures of " + newTitle
+const modifyTitle = newTitle => {
+	const title = document.getElementById("title");
+	title.innerText = "Pictures of " + newTitle;
 };
